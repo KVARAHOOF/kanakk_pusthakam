a669dea321d484a1c474ef52eafe787ff7c880f5
KVARAHOOF <abrahoofkv@gmail.com>
2025-10-12 09:14:07 +0400

    Add files via upload

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "change_this_secret"
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
-        "sqlite:///" + os.path.join(basedir, "db.sqlite3")
+        "postgresql://neondb_owner:npg_w6pRD0WnMBQJ@ep-cold-scene-af3vj731-pooler.c-2.us-west-2.aws.neon.tech/kanakk_db?sslmode=require&channel_binding=require"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
